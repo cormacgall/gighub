b5f69fd4a3a84111595aedfb939c1f6c273d844b
Cormac English <cormac.english@hpe.com>
2018-04-13 14:35:38 +0100

    Initial commit

---
+++ Models/Gig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GigHub2.Models
+{
+    public class Gig
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public ApplicationUser Artist { get; set; }//who
+
+        public DateTime DateTime { get; set; }//when
+
+        [Required]
+        [StringLength(255)]
+        public string Venue { get; set; }//where
+
+        [Required]
+        public Genre Genre { get; set; }//genre
+
+    }
+}
